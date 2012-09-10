51b608339bd325b5e4d5b14d096e5c50ff48e1fb
Peter Andersson <peter@snowfall.se>
2012-09-10 19:30:41 +0200

    new config vars

--- inc/config-example.php
+++ inc/config-example.php
@@ -4,6 +4,5 @@ define('FRECKLE_BASEURL','https://COMPANYNAME.letsfreckle.com/api/');
 define('FRECKLE_TOKEN','API_TOKEN');
 define('FRECKLE_USER','USER_EMAIL');
 define('BASE_PATH','PATH_TO_SCRIPT_DIR');
-
-
-
+define('CACHE_AGE',12);
+define('VERBOSE',1);
